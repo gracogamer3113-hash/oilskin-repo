141d076c6d56f3cbd4769cb97f01aaf4da3f5810
La Piedra <lapiedra@example.com>
2026-04-25 10:47:36 -0500

    improve: mysql fields structure

--- database/defiv.sql
+++ database/defiv.sql
@@ -11,18 +11,19 @@ CREATE TABLE categoria (
 CREATE TABLE usuario (
   id_usuario INT PRIMARY KEY AUTO_INCREMENT,
   nombre VARCHAR(50) NOT NULL,
-  contrasena VARCHAR(50) NOT NULL,
+  contrasena VARCHAR(256) NOT NULL,
   direccion VARCHAR(50) NOT NULL,
   telefono VARCHAR(15),
   celular VARCHAR(15) NOT NULL,
   email VARCHAR(80) NOT NULL UNIQUE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   id_rol INT NOT NULL,
   FOREIGN KEY (id_rol) REFERENCES rol(id_rol)
 )ENGINE=InnoDB;
 CREATE TABLE producto (
   id_producto INT PRIMARY KEY AUTO_INCREMENT,
-  nombre_producto VARCHAR(20) NOT NULL,
-  descripcion TEXT NOT NULL,
+  nombre_producto VARCHAR(150) NOT NULL,
+  descripcion TEXT,
   precio DECIMAL(10,2) NOT NULL,
   stock INT NOT NULL,
   id_categoria INT NOT NULL,
@@ -31,17 +32,19 @@ CREATE TABLE producto (
 CREATE TABLE pedido (
   id_pedido INT PRIMARY KEY AUTO_INCREMENT,
   fecha_pedido DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  estado_pedido ENUM('pendiente', 'pagado', 'cancelado') DEFAULT 'pendiente',
   subtotal DECIMAL(10,2) NOT NULL,
   id_usuario INT NOT NULL,
   FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 )ENGINE=InnoDB;
 CREATE TABLE factura (
   id_factura INT PRIMARY KEY AUTO_INCREMENT,
+  id_pedido INT NOT NULL UNIQUE,
   fecha_factura DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   subtotal DECIMAL(10,2) NOT NULL,
-  iva DECIMAL(5,2) NOT NULL DEFAULT 19.00,
   total DECIMAL(10,2) NOT NULL,
-  metodo_pago VARCHAR(20) NOT NULL
+  metodo_pago ENUM('efectivo', 'transferencia', 'tarjeta') NOT NULL,
+  FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido)
 )ENGINE=InnoDB;
 CREATE TABLE domicilio (
   id_domicilio INT PRIMARY KEY AUTO_INCREMENT,
@@ -63,4 +66,3 @@ CREATE TABLE detalle_pedido (
   FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido),
   FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
 )ENGINE=InnoDB;
-
